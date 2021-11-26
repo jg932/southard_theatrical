b830d481201103b8cddd4eef6c666b5bbd1b8367
jg932 <jared.h.goldstein@gmail.com>
2021-11-26 14:28:59 -0500

    pre first migration

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+AUTH_USER_MODEL = 'main_app.MyUser'
+
+class MyUser(AbstractUser):
+  pass
+
+class Garment(models.Model):
+  type = models.CharField
+  rate = models.CharField
+  color = models.CharField
+  genre = models.CharField
+  sizing = models.CharField
+  materials = models.CharField
+  care = models.TextField
+  notes = models.TextField
