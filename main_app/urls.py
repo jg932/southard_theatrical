985f45bf652daa8fe517a27f76563fe8dd3c4026
jg932 <jared.h.goldstein@gmail.com>
2021-11-26 13:46:23 -0500

    initially rendering

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-		
-	]
+  path('', views.home, name='home'),
+  path('about/', views.about, name='about'),
+  path('garments/', views.garments_index, name='garments_index'),
+]
--- southard_theatrical/urls.py
+++ southard_theatrical/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 
 urlpatterns = [
+    # localhost:8000/
+    path("", include('main_app.urls')),
+    # localhost:8000/admin/
     path('admin/', admin.site.urls),
-
 ]
