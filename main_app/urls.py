12445c3e64d0948f9170ea491504e150ee8be93a
jg932 <jared.h.goldstein@gmail.com>
2021-11-26 15:44:35 -0500

    detail view

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
   path('', views.home, name='home'),
   path('about/', views.about, name='about'),
   path('garments/', views.garments_index, name='garments_index'),
+  path('garments/<int:garment_id>/', views.garments_detail, name='garments_detail'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,4 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 from .models import Garment
 
@@ -11,3 +12,7 @@ def about(request):
 def garments_index(request):
   garments = Garment.objects.all()
   return render(request, 'garments/index.html', { 'garments': garments })
+
+def garments_detail(request, garment_id):
+  garment = Garment.objects.get(id=garment_id)
+  return render(request, 'garments/detail.html', {'garment': garment })
