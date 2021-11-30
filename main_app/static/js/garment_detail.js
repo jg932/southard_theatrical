efec3b23b3a9680fb19cdf5955d22a01c1c66a09
jg932 <jared.h.goldstein@gmail.com>
2021-11-30 18:25:41 -0500

    background image fix

--- main_app/static/js/garment_detail.js
+++ main_app/static/js/garment_detail.js
@@ -1,5 +1,14 @@
 const fileInput = document.getElementById('file-input')
 const fileName = document.getElementById('file-name')
+const availableButton = document.getElementById('available_button')
+const outOfStockDisplay = document.getElementById('out_of_stock_display')
+
+availableButton.addEventListener("click", outOfStock)
+
+function outOfStock(){
+  outOfStockDisplay.hidden=false
+}
+
 
 fileInput.addEventListener('change', evt => {
   const fileToUpload = evt.target.files[0].name
