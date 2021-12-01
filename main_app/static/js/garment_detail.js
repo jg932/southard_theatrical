e0067ef2d9bc5097f3c26659eef3be817d1e6476
jg932 <jared.h.goldstein@gmail.com>
2021-11-30 19:17:05 -0500

    more styling

--- main_app/static/js/garment_detail.js
+++ main_app/static/js/garment_detail.js
@@ -1,12 +1,22 @@
 const fileInput = document.getElementById('file-input')
 const fileName = document.getElementById('file-name')
+const unavailableButton = document.getElementById('unavailable_button')
 const availableButton = document.getElementById('available_button')
 const outOfStockDisplay = document.getElementById('out_of_stock_display')
 
-availableButton.addEventListener("click", outOfStock)
+unavailableButton.addEventListener("click", outOfStock)
+availableButton.addEventListener("click", backInStock)
 
 function outOfStock(){
   outOfStockDisplay.hidden=false
+  unavailableButton.hidden=true
+  availableButton.hidden=false
+}
+
+function backInStock(){
+  outOfStockDisplay.hidden=true
+  availableButton.hidden=true
+  unavailableButton.hidden=false
 }
 
 
